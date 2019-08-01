2563177a63556207ba4c50a052e42312f9234375
zython-repo <dydgur316@gmail.com>
2019-08-01 10:22:56 +0900

    fin Step3

--- Step3/bk3_11.java
+++ Step3/bk3_11.java
@@ -8,29 +8,25 @@ public class bk3_11
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int a = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-			
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		
-		String[] list = br.readLine();
-		
+		String a = br.readLine();
+		int getIt;
+
 		try{
-			if (1<=b && b<=10000 && 1<=c && c<=10000){
+			StringTokenizer st = new StringTokenizer(a, " ");
 			
-				for (int i = 0; i < list.size(); i++){
-					String result = list.get(i);
-					if (result > b){
-						list.remove(result);
-					}
-					else {
-						
-					}
+			int len = Integer.parseInt(st.nextToken());
+			int stan = Integer.parseInt(st.nextToken());
+			
+			StringTokenizer str = new StringTokenizer(br.readLine());
+
+			for (int i = 0; i < len; i++){
+				getIt = Integer.parseInt(str.nextToken());
+				
+				if (getIt < stan){
+					bw.write(getIt + " ");
 				}
-					bw.write("\n");
 			}
+			bw.write("\n");
 		}catch(Exception e){
 			bw.write("So sad.." + "\n" + e + "\n");
 		}
